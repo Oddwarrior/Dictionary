cf56c665672f13f3d64f3cbf9b0a96fec870d516
Shashank <you@example.com>
2023-01-07 00:35:11 +0530

    working! first committ

---
+++ frontend/translator/main.js
@@ -0,0 +1,47 @@
+const form = document.querySelector('form');
+const button = document.querySelector('button');
+
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    let data = new FormData(form);
+    let text = data.get('text');
+    console.log("data collected" + text);
+    // form.reset();
+
+    const response = await fetch('http://localhost:3000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            text: data.get('text')
+        })
+    });
+
+
+    if (response.ok) {
+        const { m } = await response.json()
+        // console.log(m);
+        loadMeaningOnDiv(m);
+    }
+}
+
+const loadMeaningOnDiv = async (definitions) => {
+    const meaningDiv = document.getElementById("answer");
+    meaningDiv.innerHTML = `Meaning : <br />`;
+
+    for (let i = 0; i < 2; i++) {
+        const { definition } = definitions[i];
+        console.log(definition);
+        meaningDiv.innerHTML += `<li>${definition}</li>`;
+    }
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode == 13) {
+        handleSubmit(e);
+    }
+})
+
---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', async (req, res) => {
+
+    res.status(200).send({
+        "message": "Hello from shashank"
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const text = req.body.text;
+        axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`)
+            .then((response) => {
+                // const meaning1 = response.data[0].meanings[0].definitions[0]?.definition;
+                // const meaning2 = response.data[0].meanings[0].definitions[1]?.definition;
+
+                const meaning = response.data[0].meanings[0].definitions;
+
+                res.status(200).send({
+                    m: meaning
+                });
+            })
+
+        // res.status(200).send(response);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error });
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log("Server  is running on port 3000 ");
+})
